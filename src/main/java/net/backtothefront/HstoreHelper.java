f4b9f7b05564659f4929cb5b305d842724a143a9
AJ DaNelz <andrew.danelz@m33integrated.com>
2014-12-14 20:55:35 -0500

    Multiple updates: added special character escaping, added support for commas in values. removed possible index out of bounds errors, cleaned up code.

--- src/main/java/net/backtothefront/HstoreHelper.java
+++ src/main/java/net/backtothefront/HstoreHelper.java
@@ -8,9 +8,11 @@ import java.util.Map;
 
 // courtesy of: http://backtothefront.net/2011/storing-sets-keyvalue-pairs-single-db-column-hibernate-postgresql-hstore-type/
 public class HstoreHelper {
-
+    
     private static final String K_V_SEPARATOR = "=>";
-
+    private static final String ATTR_SEPARATOR = ", ";
+    private static final String COMMA_ESCAPE = "&com;";
+    
     public static String toString(Map<String, String> m) {
         if (m.isEmpty()) {
             return "";
@@ -18,27 +20,37 @@ public class HstoreHelper {
         StringBuilder sb = new StringBuilder();
         int n = m.size();
         for (String key : m.keySet()) {
-            sb.append("\"" + key + "\"" + K_V_SEPARATOR + "\"" + m.get(key) + "\"");
+            //replace the ATTR_SEPARATOR with a COMMA_ESCAPE and also escape any special chars with a \ so that sql with be happy
+            sb.append("\"").append(key).append("\"").append(K_V_SEPARATOR).append("\"").append(m.get(key).replaceAll(ATTR_SEPARATOR, COMMA_ESCAPE).replaceAll("(?=[],\\[+&|!(){}^\"~*?:\\\\-])", "\\\\")).append("\"");
             if (n > 1) {
-                sb.append(", ");
+                sb.append(ATTR_SEPARATOR);
                 n--;
             }
         }
         return sb.toString();
     }
-
+    
     public static Map<String, String> toMap(String s) {
-        Map<String, String> m = new HashMap<String, String>();
+        Map<String, String> m = new HashMap<>();
         if (! StringUtils.hasText(s)) {
             return m;
         }
-        String[] tokens = s.split(", ");
+        String[] tokens = s.split(ATTR_SEPARATOR);
         for (String token : tokens) {
             String[] kv = token.split(K_V_SEPARATOR);
             String k = kv[0];
+            String v = kv[1].replace(COMMA_ESCAPE, ATTR_SEPARATOR).trim();
+            
+            if(k.length() == 0){
+                break;
+            }
+            
             k = k.trim().substring(1, k.length() - 1);
-            String v = kv[1];
-            v = v.trim().substring(1, v.length() - 1);
+            if(!v.toLowerCase().equals("null") && v.length() > 1){
+                v = v.substring(1, v.length() - 1);
+            }else{
+                v = null;
+            }
             m.put(k, v);
         }
         return m;
